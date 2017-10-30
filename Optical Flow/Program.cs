1c32f27c757e8414ccdb95ec652911deec1dfa02
Tim Fiss <timothy.fiss@usask.ca>
2017-10-30 11:25:17 -0600

    added a basic layout for the form

--- Optical Flow/Program.cs
+++ Optical Flow/Program.cs
@@ -8,18 +8,22 @@ namespace Optical_Flow
 {
     class Program
     {
-        static readonly string Version = "0.1.1";
+        static readonly string Version = "0.2.1";
+        [STAThread]
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to the optic flow tool Version {0}, Please note this ONLY works with .bmp images", Version);
-            Console.WriteLine("Please specify the filepath for the video");
 
+            /*
             Console.WriteLine("Running");
             //Hard Code the URL just for funsies
             var video = @"D:\sample.mp4"; //TODO: Make dynamic.
             VideoToBitmap.RunOnVideo(video, 4, 8);
+            */
+            MainForm form = new MainForm();
+            form.ShowDialog();
             Console.Read();
+            
         }
     }
 }
